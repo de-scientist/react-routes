5182951864099395088bf089fd785cdd0d648d5d
de-scientist <gitaumark502@gmail.com>
2025-11-17 12:01:49 +0300

    very important: <Route path="/" element={<h1>You are loved and blessed</h1>} />

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,14 @@ function App() {
     <>
      <div>
     <Routes>
+      <Route path="/" element={<h1>You are loved and blessed</h1>} />
       <Route path="/about" element={<h1>About pages</h1>} />
       <Route path="/products" element={<h1>Products</h1>} />
       <Route path="/team" element={<h1>Team</h1>} />
     </Routes>
      </div>
     </>
-  )
+  );
 }
 
 export default App
