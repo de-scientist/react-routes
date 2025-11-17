16096e54b4d2336354ffcb1d0379d4c874e30329
de-scientist <gitaumark502@gmail.com>
2025-11-17 12:16:40 +0300

    add a footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes,Route } from "react-router-dom"
+import { Routes, Route, Link } from "react-router-dom"
 
 function App() {
   
@@ -6,12 +6,18 @@ function App() {
   return (
     <>
      <div>
+      <header>
+
+      </header>
     <Routes>
       <Route path="/" element={<h1>You are loved and blessed</h1>} />
       <Route path="/about" element={<h1>About pages</h1>} />
       <Route path="/products" element={<h1>Products</h1>} />
       <Route path="/team" element={<h1>Team</h1>} />
     </Routes>
+    <footer>
+      &copy proffesionally designed by De-scientist
+    </footer>
      </div>
     </>
   );
---
+++ src/pages/Products.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Products() {
+  return (
+    <div>
+<h1>Our Products</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, dolorum vero? Debitis voluptatum consequatur mollitia sapiente beatae adipisci doloremque commodi fugit quidem ullam. Iure delectus error quis adipisci facilis veritatis!</p>
+    </div>
+  )
+}
+
+export default Products
